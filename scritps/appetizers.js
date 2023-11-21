958b32b6a0e4045bb7ae966beea66a9957e02d18
llariviereve3lgu@outlook.com <23037746@saultcollege.ca>
2023-11-20 19:34:43 -0500

    update

--- scritps/appetizers.js
+++ scritps/appetizers.js
@@ -5,7 +5,13 @@ const Appetizer = (value) => {
     const appetizerH2 = document.createElement('h2');
     appetizerH2.innerHTML = 'Appetizers'
     appetizer.appendChild(appetizerH2)
+    const appHead = document.createElement('h1');
+  appHead.setAttribute('class', 'text-center my-3 pt-5 mb-5');
+  appHead.innerHTML = 'APPITIZERS';
     
+    
+    const app1 = document.createElement('h1');
+    app1.setAttribute('id', 'app1');
     return appetizer;
 };
 export default Appetizer;

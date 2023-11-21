9a50ef6968ab2eb0192c06c7609962e9ad065aba
llariviereve3lgu@outlook.com <23037746@saultcollege.ca>
2023-11-20 20:04:57 -0500

    Update appetizers.js

--- scritps/appetizers.js
+++ scritps/appetizers.js
@@ -1,17 +1,55 @@
-const Appetizer = (value) => {
-    console.log(value)
-    const appetizer = document.createElement('div');
-    appetizer.setAttribute('class', 'container');
-    const appetizerH2 = document.createElement('h2');
-    appetizerH2.innerHTML = 'Appetizers'
-    appetizer.appendChild(appetizerH2)
-    const appHead = document.createElement('h1');
-  appHead.setAttribute('class', 'text-center my-3 pt-5 mb-5');
-  appHead.innerHTML = 'APPITIZERS';
-    
-    
-    const app1 = document.createElement('h1');
-    app1.setAttribute('id', 'app1');
-    return appetizer;
-};
-export default Appetizer;
+const Appetizer = () => {
+    const menuContent = document.createElement('div');
+    menuContent.setAttribute('class', 'container');
+  
+    const menuHeader = document.createElement('h1');
+    menuHeader.setAttribute('class', 'text-center my-3 pt-5 mb-5');
+    menuHeader.innerHTML = 'Appetizers';
+  
+    const menuList = document.createElement('ul');
+    menuList.setAttribute('class', 'menuList');
+  
+    const menuItems = () => {
+      const list = document.createElement('li');
+      list.setAttribute('class', 'nav-item list-unstyled mx-2');
+  
+      const menu1 = document.createElement('h1');
+      menu1.setAttribute('id', 'app1');
+      menu1.setAttribute('class', 'text-center');
+      menu1.innerHTML = 'Mozza Sticks';
+      list.appendChild(menu1);
+      const menuParagraph1 = document.createElement('p');
+      menuParagraph1.setAttribute('class', 'my-3 pb-3');
+      menuParagraph1.innerHTML = '';
+      list.appendChild(menuParagraph1);
+  
+      const menu2 = document.createElement('h1');
+      menu2.setAttribute('id', 'menu2');
+      menu2.setAttribute('class', 'text-center');
+      menu2.innerHTML = 'BARBECUE';
+      list.appendChild(menu2);
+      const menuParagraph2 = document.createElement('p');
+      menuParagraph2.setAttribute('class', 'my-3 pb-3');
+      menuParagraph2.innerHTML = 'Full Rack. Slow-cooked to fall off the bone tenderness. Slathered with your choice of sauce: - Spicy Texas BBQ sauce - Honey BBQ sauce';
+      list.appendChild(menuParagraph2);
+  
+      const menu3 = document.createElement('h1');
+      menu3.setAttribute('id', 'menu3');
+      menu3.setAttribute('class', 'text-center');
+      menu3.innerHTML = 'FISH';
+      list.appendChild(menu3);
+      const menuParagraph3 = document.createElement('p');
+      menuParagraph3.setAttribute('class', 'my-3 pb-3');
+      menuParagraph3.innerHTML = 'Blackened salmon fillet grilled to perfection. 6 oz. Served with your choice of two sides.';
+      list.appendChild(menuParagraph3);
+  
+      return list;
+    };
+  
+    menuContent.appendChild(menuHeader);
+    menuContent.appendChild(menuItems());
+  
+    return menuContent;
+  };
+  
+  export default Appetizer;

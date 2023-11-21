09413670a149d8d3ad25dc1a8e4b087766b71183
llariviereve3lgu@outlook.com <23037746@saultcollege.ca>
2023-11-20 19:22:11 -0500

    added appetizers

--- scritps/index.js
+++ scritps/index.js
@@ -2,7 +2,7 @@ import Home from './home.js';
 import Menu from './menu.js';
 import Navbar from './navbar.js';
 import Contact from './contact.js';
-
+import Appetizer from './appetizers.js';
 const Index = () => {
   function italicsBody() {
     document.body.style.fontFamily = "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif";
@@ -14,11 +14,13 @@ const Index = () => {
   content.appendChild(navbar);
   const home = Home();
   content.appendChild(home);
+
   
   // NAVBAR CLICKING
   
   const menu = Menu();
   const contact = Contact();
+  const appetizer = Appetizer();
   
   const link1 = document.getElementById('home');
   link1.addEventListener('click', () => {
@@ -28,6 +30,11 @@ const Index = () => {
   const link2 = document.getElementById('menu');
   link2.addEventListener('click', () => {
     content.replaceChild(menu, content.childNodes[1]);
+  });
+
+  const link4 = document.getElementById('appetizer');
+  link4.addEventListener('click', () => {
+    content.replaceChild(appetizer, content.childNodes[1]);
   });
   
   const link3 = document.getElementById('contact');
